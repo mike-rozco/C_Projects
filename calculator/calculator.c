af360c1b5396d7e6e1235a6ae9276df41b6a8d17
mike-el-fenixxx <mike.el.fenixxx@gmail.com>
2023-10-16 14:03:12 -0400

    Formatted the file correctly with the clang-format command

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -1,69 +1,60 @@
 #include <stdio.h>
 
+int main(int argc, char* argv[]) {
+  // This declares the variables for the user input and the two numbers to
+  // perform a calculation on.
+  int choice;
+  int x, y;
+  int c;
 
-int main (int argc, char* argv[]) {
+  for (;;) {
+    printf("Please make a selection: \n");
+    printf("1. Add \n");
+    printf("2. Subtract \n");
+    printf("3. Multiply \n");
+    printf("4. Divide \n");
+    printf("5. Quit \n");
+    scanf("%d", &choice);
 
+    if (choice == 5) {
+      printf("Exiting the program.\n");
+      break;  // Exit the while loop
+    }
 
-//This declares the variables for the user input and the two numbers to perform a calculation on.	
-int choice;
-int x, y;
-int c;
+    printf("Enter first number: \n");
+    scanf("%d", &x);
+    printf("Enter second number: \n");
+    scanf("%d", &y);
 
+    while ((c = getchar()) != '\n') {
+    };  // Clear the buffer
 
-for(;;) {
-
-  printf("Please make a selection: \n");
-  printf("1. Add \n");
-  printf("2. Subtract \n");
-  printf("3. Multiply \n");
-  printf("4. Divide \n");
-  printf("5. Quit \n");
-  scanf("%d", &choice);
-
-  if (choice == 5)
-  {
-    printf("Exiting the program.\n");
-    break; // Exit the while loop
+    // Can create a more modular approach by fitting each operation into a
+    // function
+    //  but for four simple operations a switch statement suffices.
+    switch (choice) {
+      case 1:
+        printf("Total: %d \n", x + y);
+        break;
+      case 2:
+        printf("Total: %d \n", x - y);
+        break;
+      case 3:
+        printf("Total: %d \n", x * y);
+        break;
+      case 4:
+        if (y == 0) {
+          printf("Error: Divsion by zero!\n");
+          break;
+        } else {
+          printf("Total: %d \n", x / y);
+        }
+        break;
+      default:
+        printf("Invalid selection. \n");
+        break;
+    }
   }
 
-  
-  printf("Enter first number: \n");
-  scanf("%d", &x);
-  printf("Enter second number: \n");
-  scanf("%d", &y);
-
-
-
-while((c = getchar()) != '\n') { }; // Clear the buffer
-
-
-//Can create a more modular approach by fitting each operation into a function
-// but for four simple operations a switch statement suffices.
-switch (choice) {
-  case 1: 
-    printf("Total: %d \n", x + y);
-    break;
-  case 2:
-    printf("Total: %d \n", x - y);
-    break;
-  case 3:
-    printf("Total: %d \n", x * y);
-    break;
-  case 4:
-    if (y == 0) {
-	      printf("Error: Divsion by zero!\n");
-      break;
-    } else {
-        printf("Total: %d \n", x / y);
-    } 
-    break;
-  default:
-    printf("Invalid selection. \n");
-    break;
-  }
-
-}
-
-
-return 0;
+  return 0;
 }
